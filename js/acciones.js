278f36dd4e74878f9522fe008df38204ca0b922d
canetemendez <marina3f.dsaupoo@gmail.com>
2016-05-24 19:57:21 -0500

    3subida

--- js/acciones.js
+++ js/acciones.js
@@ -10,7 +10,7 @@ $(document).ready(function(e) {
 audio.preloadFX('do', 'audio/C.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
 audio.preloadFX('re', 'audio/D.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
 audio.preloadFX('mi', 'audio/E.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-audio.preloadFX('fa', 'audio/f.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+audio.preloadFX('fa', 'audio/F.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
 audio.preloadFX('sol', 'audio/G.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
 audio.preloadFX('la', 'audio/A.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
 audio.preloadFX('si', 'audio/B.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
